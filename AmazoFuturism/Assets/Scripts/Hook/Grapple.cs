8fd03b1134fedf1ca427f48056a0241caf3a46ae
Renato Avelino <43862847+RenatoAvelino@users.noreply.github.com>
2023-06-17 18:59:58 -0300

    Gancho Base

---
+++ AmazoFuturism/Assets/Scripts/Hook/Grapple.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapple : MonoBehaviour
+{
+    [SerializeField]
+    private float pullSpeed = 4f;
+    [SerializeField]
+    private float stopDistance = .5f;
+    [SerializeField]
+    private GameObject hookPrefab;
+    [SerializeField]
+    private Transform shootTransform;
+
+    private Hook hook;
+    private bool pulling;
+    private Rigidbody rigid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        pulling = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hook == null && Input.GetMouseButtonDown(0))
+        {
+            //StopAllCoroutines();
+            Vector3 worldPos3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 worldPos2D = new Vector3(worldPos3D.x, worldPos3D.y, this.gameObject.transform.position.z);
+            shootTransform.LookAt(worldPos2D);
+            pulling = false;
+            hook = Instantiate(hookPrefab, shootTransform.position, Quaternion.identity).GetComponent<Hook>();
+            hook.Initialize(this, shootTransform);
+            StartCoroutine(DestroyHookAfterLifetime());
+        }
+        else if (hook != null && Input.GetMouseButtonDown(1))
+        {
+            DestroyHook();
+        }
+
+        if (!pulling || hook == null) return;
+
+        if(Vector3.Distance(transform.position, hook.transform.position) <= stopDistance)
+        {
+            DestroyHook();
+        }
+        else
+        {
+            rigid.AddForce((hook.transform.position - transform.position).normalized * pullSpeed,
+                ForceMode.VelocityChange);
+        }
+    }
+
+    public void StartPull()
+    {
+        pulling = true;
+    }
+
+    private void DestroyHook()
+    {
+        if (hook == null) return;
+
+        pulling = false;
+        Destroy(hook.gameObject);
+        hook = null;
+    }
+
+    private IEnumerator DestroyHookAfterLifetime()
+    {
+        yield return new WaitForSeconds(8f);
+
+        DestroyHook();
+    }
+}
---
+++ AmazoFuturism/Assets/Scripts/Hook/Hook.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    [SerializeField]
+    private float hookForce = 25f;
+
+    private Grapple grappler;
+    private Rigidbody rigid;
+    private LineRenderer lineRenderer;
+
+    public void Initialize(Grapple grapple, Transform shootTransform)
+    {
+        transform.forward = shootTransform.forward;
+        grappler = grapple;
+        rigid = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
+        rigid.AddForce(transform.forward * hookForce, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] positions = new Vector3[]
+        {
+            transform.position,
+            grappler.transform.position
+        };
+        lineRenderer.SetPositions(positions);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if((LayerMask.GetMask("Grapple") & 1 << other.gameObject.layer ) > 0)
+        {
+            rigid.useGravity = false;
+            rigid.isKinematic = true;
+
+            grappler.StartPull();
+        }
+    }
+}
