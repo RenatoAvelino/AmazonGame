69d7c6c47dc0396a94808dec6987f36dafccb274
Joaomelov <joaoguilhermemv@gmail.com>
2023-07-27 18:29:13 -0300

    UI  02
    Consegui ajeitar o pause

--- AmazoFuturism/Assets/UI/Scripts/PauseMenu.cs
+++ AmazoFuturism/Assets/UI/Scripts/PauseMenu.cs
@@ -9,6 +9,11 @@ public class PauseMenu : MonoBehaviour
 
   public GameObject pauseMenuUI;
 
+  void Start()
+  {
+    Resume();
+  }
+
     // Update is called once per frame
     void Update()
     {
